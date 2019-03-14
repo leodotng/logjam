186a425009f22673af5ebe0d0310d5b32ff93bc1
Josh Leong <josh@leo.ng>
2019-03-14 00:20:36 -0600

    added top nav

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import { Alert } from "shards-react";
+import { Alert, Button } from "shards-react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css";
+import FormExample from './components/FormExample';
+import NavExample from './components/NavExample';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <NavExample />
         <h1>LogJam</h1>
         
         <Alert theme="primary">
         Get Unstuck!
       </Alert>
       <h2>When projects break down use logjam to get unstuck!</h2>
+      Add Team Member
+      <FormExample />
+      Add Team Member # 2
+      <FormExample />
+      <Button>Submit</Button>
+      <Button theme="danger">Cancel</Button>
       </div>
     );
   }
---
+++ src/components/FormExample.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Form, FormInput } from "shards-react";
+
+export default function FormExample() {
+  return (
+    <Form>
+      <FormInput size="lg" placeholder="Large input" />
+    </Form>
+  );
+}
