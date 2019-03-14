63731a2cddd729b554a9c140ba03a62ad2382a51
Josh Leong <josh@leo.ng>
2019-03-14 00:02:18 -0600

    add shards

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Alert } from "shards-react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>LogJam</h1>
-        <h2>When projects break down use logjam to get unstuck!</h2>
+        
+        <Alert theme="primary">
+        Get Unstuck!
+      </Alert>
+      <h2>When projects break down use logjam to get unstuck!</h2>
       </div>
     );
   }
